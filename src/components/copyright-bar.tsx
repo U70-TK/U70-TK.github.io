35d2c0a689a420e4b7bdec6781bce3edef79718c
U70-TK <wtkuan@163.com>
2026-01-07 15:47:38 -0500

    footer

--- src/components/copyright-bar.tsx
+++ src/components/copyright-bar.tsx
@@ -1,32 +1,25 @@
-import {
-    Box,
-    Typography 
-} from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 const CopyRightBar = () => {
-    return (
-        <Box
-            sx={{
-                width: "100%",
-                height: "3rem",
-                backgroundColor: "#000000",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-            }}
-        >
-            <Typography
-                variant="body2"
-                sx={{
-                    color: "#ffffff",
-                    opacity: 0.8,
-                    fontSize: "0.9rem",
-                }}
-            >
-                © {new Date().getFullYear()} Felix (Tiankuan) Wang. All rights reserved.
-            </Typography>
-        </Box>
-    );
-}
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "3rem",
+        backgroundColor: "#000000",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Typography
+        variant="body2"
+        sx={{ color: "#ffffff", opacity: 0.8, fontSize: "0.9rem" }}
+      >
+        © {new Date().getFullYear()} Felix (Tiankuan) Wang. All rights reserved.
+      </Typography>
+    </Box>
+  );
+};
 
 export default CopyRightBar;
